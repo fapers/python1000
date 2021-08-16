2bce18eb9d55f59f819bc9c1767d38c2f4de6b96
Fábio Pereira de Souza <fpsmoc@yahoo.com.br>
2021-08-16 14:39:27 -0300

    instruções

---
+++ teste.py
@@ -0,0 +1,6 @@
+print('\n'*100)
+my_var = 'Hello World!'
+print(my_var)
+print('\n'*10)
+my_var = 'Hello World, two!'
+help(print)
---
+++ teste1.py
@@ -0,0 +1,24 @@
+##!/usr/bin/env python3
+
+my_var = "New String"
+print(my_var)
+dir()
+
+def foo():
+    var1 = 1
+    var2 = 2
+
+def foo():
+    var1 = 1
+    var2 = 2
+   
+def teste():
+    try:
+        optionA = int(input('Enter A: '))
+    except:
+        optionA = 0
+    optionB = int(input('Enter B: '))
+    return optionA + optionB
+
+print(teste())
+   
